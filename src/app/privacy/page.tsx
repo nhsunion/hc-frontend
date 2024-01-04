e49b909a8c30b7f7ed115f237479979aa9458250
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2024-01-04 19:44:44 +0100

    initial privacy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import Image from "next/image"
+
+
+export default function PrivacyPolicy() {
+    return (
+        <article>
+            <h1 className="text-5xl p-8">Privacy Policy</h1>
+        </article>
+    )
+}
