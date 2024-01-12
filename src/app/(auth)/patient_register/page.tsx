46f0d9dd0de20209463670b1104c1d407a862359
md-jafari <112261558+md-jafari@users.noreply.github.com>
2024-01-12 20:08:41 +0100

    Connected patient register to backend

--- src/app/(auth)/patient_register/page.tsx
+++ src/app/(auth)/patient_register/page.tsx
@@ -2,8 +2,39 @@
 import { Button } from "@mui/material"
 import Link from "next/link"
 import { button_style } from "../../utils/Styling"
+import { useRouter } from "next/navigation"
+import { FormEvent, useState } from "react"
+
 
 export default function RegisterForm() {
+    const router = useRouter()
+  const [fullName, setFullName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/register`, {
+      body: JSON.stringify({
+        fullName: fullName,
+        email: email,
+        password: password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      credentials: 'include'
+    })
+
+    if (res.status === 200) {
+      const token = await res.json()
+      router.push('/login')
+    }
+    else {
+      alert("Error")
+    }
     return (
         <div className="bg-grey-lighter min-h-screen flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
@@ -11,14 +42,17 @@ export default function RegisterForm() {
                     <h1 className="mb-8 text-3xl text-center">Sign up</h1>
                     <input
                         type="text"
+                        onChange={e => setFullName(e.target.value)}
                         className="block border border-grey-light w-full p-3 rounded mb-4"
                         placeholder="Full Name" />
                     <input
                         type="email"
+                        onChange={e => setEmail(e.target.value)}
                         className="block border border-grey-light w-full p-3 rounded mb-4"
                         placeholder="Email" />
                     <input
                         type="password"
+                        onChange={e => setPassword(e.target.value)}
                         className="block border border-grey-light w-full p-3 rounded mb-4"
                         placeholder="Password" />
                     <input
@@ -27,6 +61,7 @@ export default function RegisterForm() {
                         placeholder="Confirm Password" />
                     <Button
                         variant="contained"
+                        onClick={handleSubmit}
                         style={button_style}
                         className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
                     >Create Account</Button>
@@ -40,4 +75,5 @@ export default function RegisterForm() {
             </div>
         </div>
     )
+}
 }
