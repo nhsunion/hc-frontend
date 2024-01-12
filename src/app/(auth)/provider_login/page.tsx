b952f38b36fab00c23db3b0acc62076ea00a7d9a
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2024-01-12 18:36:52 +0100

    navbar and provider login

---
+++ src/app/(auth)/provider_login/page.tsx
@@ -0,0 +1,127 @@
+'use client'
+import Logo from "@/app/components/logo"
+import NavBar from "@/app/components/navbar"
+import { button_style } from "@/app/utils/Styling"
+import { Button, Link } from "@mui/material"
+import { useRouter } from "next/navigation"
+import { FormEvent, useState } from "react"
+
+export default function ProviderLogin() {
+    const router = useRouter()
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/provider/login`, {
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            credentials: 'include'
+        })
+
+        if (res.status === 200) {
+            router.push('/provider_page')
+        }
+        else {
+            alert("Wrong username or password")
+        }
+    }
+
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <NavBar currentPath="/provider_login" />
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <Logo />
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Provider Login
+                        </h1>
+                        <form className="space-y-4 md:space-y-6">
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Username
+                                </label>
+                                <input
+                                    type="text"
+                                    onChange={e => setUsername(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Username or Email"
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    onChange={e => setPassword(e.target.value)}
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                />
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input
+                                            id="remember"
+                                            aria-describedby="remember"
+                                            type="checkbox"
+                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                        />
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label
+                                            htmlFor="remember"
+                                            className="text-gray-500 dark:text-gray-300"
+                                        >
+                                            Remember me
+                                        </label>
+                                    </div>
+                                </div>
+                                <a
+                                    href="#"
+                                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                >
+                                    Forgot password?
+                                </a>
+                            </div>
+                            <Button
+                                variant="contained"
+                                style={button_style}
+                                onClick={handleSubmit}
+                            >
+                                Login</Button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Don&apos;t have an account yet?{" "}
+                                <Link href="/provider_register"
+                                    className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                                    Sign up
+                                </Link>
+                            </p>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                By signing up, you agree to our{" "}
+                                <Link href="/privacy"
+                                    className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                                    Privacy Policy
+                                </Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
