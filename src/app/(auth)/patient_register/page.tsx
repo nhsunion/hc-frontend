97867a8a66357024aae3e3f8d8be7c2a6dba32a5
md-jafari <112261558+md-jafari@users.noreply.github.com>
2024-01-12 22:06:56 +0100

    fixed syntax

--- src/app/(auth)/patient_register/page.tsx
+++ src/app/(auth)/patient_register/page.tsx
@@ -33,45 +33,45 @@ export default function RegisterForm() {
         else {
             alert("Error")
         }
-        return (
-            <div className="bg-grey-lighter min-h-screen flex flex-col">
-                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                    <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                        <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-                        <input
-                            type="text"
-                            onChange={e => setFullName(e.target.value)}
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            placeholder="Full Name" />
-                        <input
-                            type="email"
-                            onChange={e => setEmail(e.target.value)}
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            placeholder="Email" />
-                        <input
-                            type="password"
-                            onChange={e => setPassword(e.target.value)}
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            placeholder="Password" />
-                        <input
-                            type="password"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            placeholder="Confirm Password" />
-                        <Button
-                            variant="contained"
-                            onClick={handleSubmit}
-                            style={button_style}
-                            className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-                        >Create Account</Button>
-                    </div>
-                    <div className="text-grey-dark mt-6">
-                        Already have an account?
-                        <Link className="no-underline border-b border-blue text-blue" href="/login">
-                            Log in
-                        </Link>.
-                    </div>
+    }
+    return (
+        <div className="bg-grey-lighter min-h-screen flex flex-col">
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+                    <input
+                        type="text"
+                        onChange={e => setFullName(e.target.value)}
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        placeholder="Full Name" />
+                    <input
+                        type="email"
+                        onChange={e => setEmail(e.target.value)}
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        placeholder="Email" />
+                    <input
+                        type="password"
+                        onChange={e => setPassword(e.target.value)}
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        placeholder="Password" />
+                    <input
+                        type="password"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        placeholder="Confirm Password" />
+                    <Button
+                        variant="contained"
+                        onClick={handleSubmit}
+                        style={button_style}
+                        className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                    >Create Account</Button>
+                </div>
+                <div className="text-grey-dark mt-6">
+                    Already have an account?
+                    <Link className="no-underline border-b border-blue text-blue" href="/login">
+                        Log in
+                    </Link>.
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
