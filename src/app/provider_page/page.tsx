abf83b4008c8d26fd9ac8c8c2d728e557412e73a
md-jafari <112261558+md-jafari@users.noreply.github.com>
2024-01-23 20:19:20 +0100

    Syntax changes

--- src/app/provider_page/page.tsx
+++ src/app/provider_page/page.tsx
@@ -1,24 +1,39 @@
 'use client'
 import { SetStateAction, useEffect, useState } from "react"
-import { Patient } from "../utils/interface"
+import { Patient, UserRole } from "../utils/interface"
 import Logo from "../components/logo"
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
+import { useRouter } from "next/navigation"
 
 export default function ProviderPage() {
     const [patients, setPatients] = useState<Patient[]>([])
+    const [user, setUser] = useState<UserRole>()
+    const router = useRouter()
     
     useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/current`) // TODO: create api folder and move the fetch there
-            .then(response => response.json())
-            .then(data => setPatients(data))
-            .catch(error => console.log(error))
-    }, [])
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/current`, {
+            method: "GET",
+            credentials: "include"
+        })
+            .then((response) => response.json())
+            .then((data: UserRole) => {
+                setUser(data)
+                if (!data) {
+                    router.push("/")
+                }
+            })
+            .catch((error) => {
+                console.log("recieved error: ", error)
+                router.push("/")
+            })
+    }, []) 
+
     return (
         <div>
             <Logo />
-            <h1 className="text-5xl p-5">Welcome:</h1>
+            <h1 className="text-5xl p-5">Welcome: { user?.role}</h1>
             {patients.map((patient, index) => (
                 <div key={index} className="text-3xl">
                     {patient.name}
