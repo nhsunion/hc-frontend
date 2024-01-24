f5c1387054f5aad3dad6002a8750303fe18c9453
md-jafari <112261558+md-jafari@users.noreply.github.com>
2024-01-24 17:56:06 +0100

    Initial fetch appointment

--- src/app/patient_page/page.tsx
+++ src/app/patient_page/page.tsx
@@ -1,53 +1,78 @@
 'use client'
-import { SetStateAction, useEffect, useState } from "react"
-import { Patient, UserRole } from "../utils/interface"
+import { FormEvent, SetStateAction, useEffect, useState } from "react"
+import { Patient, UserRole, Appointment } from "../utils/interface"
 import Logo from "../components/logo"
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
 import { useRouter } from "next/navigation"
 
+
+
 export default function PatientPage() {
     const [patients, setPatients] = useState<Patient[]>([])
     const [user, setUser] = useState<UserRole>()
     const router = useRouter()
-    
-    useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/current`, {
-            method: "GET",
-            credentials: "include"
-        })
-            .then((response) => response.json())
-            .then((data: UserRole) => {
-                setUser(data)
-                if (!data) {
-                    router.push("/")
-                }
-            })
-            .catch((error) => {
-                console.log("recieved error: ", error)
-                router.push("/")
-            })
-    }, []) 
 
-    return (
-        <div>
-            <Logo />
-            <h1 className="text-5xl p-5">Welcome: { user?.role}</h1>
-            {patients.map((patient, index) => (
-                <div key={index} className="text-3xl">
-                    {patient.name}
+    const AppointmentsList = () => {
+        const [appointments, setAppointments] = useState([]);
+
+        const fetchAppointments = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/available`)
+                if (response.ok) {
+                    const data = await response.json();
+                    setAppointments(data);
+                } else {
+                    console.error('Error fetching appointments:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching appointments:', error);
+            }
+        }
+
+        useEffect(() => {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/current`, {
+                method: "GET",
+                credentials: "include"
+            })
+                .then((response) => response.json())
+                .then((data: UserRole) => {
+                    setUser(data)
+                    if (!data) {
+                        router.push("/")
+                    }
+                })
+                .catch((error) => {
+                    console.log("recieved error: ", error)
+                    router.push("/")
+                })
+
+            fetchAppointments();
+        }, [])
+
+
+        return (
+            <div>
+                <Logo />
+                <h1 className="text-5xl p-5">Welcome: {user?.role}</h1>
+                {patients.map((patient, index) => (
+                    <div key={index} className="text-3xl">
+                        {patient.name}
+                    </div>
+                ))}
+                <div>
+                    <h1>Available Appointments</h1>
+                    <button onClick={fetchAppointments}>Fetch Appointments</button>
+                    <ul>
+                        {appointments.map((appointment: Appointment) => (
+                            <li key={appointment.id}>
+                                {`Date: ${appointment.date}, Provider: ${appointment.providerName}`}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-            ))}
-            <LocalizationProvider
-                dateAdapter={AdapterDayjs}
-                localeText={{
-                    calendarWeekNumberHeaderText: '#',
-                    calendarWeekNumberText: (weekNumber) => `${weekNumber}.`,
-                }}
-            >
-                <DateCalendar displayWeekNumber />
-            </LocalizationProvider>
-        </div>
-    )
+            </div>
+        )
+    }
 }
